d2a9e5b43e4fe7a4e4e298147946d0edf799aee4
exa-ffxiv <126515697+exa-ffxiv@users.noreply.github.com>
2026-09-03 22:24:38 -0500

    Cleanup PatternTab code

--- AetherTouch/UI/Tabs/PatternTab.cs
+++ AetherTouch/UI/Tabs/PatternTab.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Numerics;
 using System.Text;
 using Dalamud.Bindings.ImGui;
+using static FFXIVClientStructs.FFXIV.Client.Sound.ISoundData.Delegates;
 
 namespace AetherTouch.UI.Tabs
 {
@@ -26,142 +27,166 @@ namespace AetherTouch.UI.Tabs
                 ImGui.TableNextRow(ImGuiTableRowFlags.None, -1f);
 
                 ImGui.TableSetColumnIndex(0);
-                if (ImGui.Button("New Pattern"))
-                {
-                    Pattern p = new Pattern([
-                            new Step(500, 25),
-                                        new Step(2000, 100),
-                                        new Step(500, 0),
-                                        new Step(500, 25)
-                            ], "Testing 1");
-                    dataManager.SavePattern(p);
-                    selectedPatternIndex = dataManager.Patterns.IndexOf(p);
-                }
-                if (ImGui.BeginListBox("##PaternList", new Vector2(200f, ImGui.GetWindowHeight() - 85)))
-                {
-                    for (var i = 0; i < dataManager.Patterns.Count; i++)
-                    {
-                        var pattern = dataManager.Patterns[i];
-                        bool isSelected = selectedPatternIndex == i;
-
-                        string label = $"{pattern.Name}##{pattern.Id}";
-                        if (ImGui.Selectable(label, isSelected))
-                        {
-                            selectedPatternIndex = i;
-                        }
-
-                        if (isSelected)
-                        {
-                            ImGui.SetItemDefaultFocus();
-                        }
-                    }
-                    ImGui.EndListBox();
-                }
+                DrawPatternList();
 
                 ImGui.TableSetColumnIndex(1);
-                if (selectedPatternIndex > -1 && selectedPatternIndex < dataManager.Patterns.Count)
-                {
-                    var selected = dataManager.Patterns[selectedPatternIndex];
-                    var name = selected.Name;
-                    var id = selected.Id.ToString();
-
-                    // Pattern details
-                    ImGui.SetNextItemWidth(250f);
-                    ImGui.InputText("Id", ref id, 100, ImGuiInputTextFlags.ReadOnly);
-                    ImGui.SetNextItemWidth(250f);
-                    if (ImGui.InputText("Name", ref name, 40))
-                    {
-                        selected.Name = name;
-                        dataManager.SavePattern(selected);
-                    }
-
-                    ImGui.BeginChild("PatternDetailsChild", new Vector2(0, ImGui.GetWindowHeight() - 150), true);
-                    ImGui.SetCursorScreenPos(ImGui.GetCursorScreenPos() + new Vector2(0.0f, 6.0f));
-
-                    // Steps
-                    for (var i = 0; i < selected.Steps.Count; i++)
-                    {
-                        var step = selected.Steps[i];
-                        var intensity = step.Intensity;
-                        var duration = step.Duration;
-                        var minPos = ImGui.GetCursorScreenPos();
-
-                        ImGui.BeginGroup();
-                        if (i == 0) ImGui.BeginDisabled();
-                        if (ImGui.Button("^"))
-                        {
-                            var temp = selected.Steps[i - 1];
-                            selected.Steps[i - 1] = step;
-                            selected.Steps[i] = temp;
-                        }
-                        if (i == 0) ImGui.EndDisabled();
-                        ImGui.SameLine();
-                        ImGui.SetNextItemWidth(100f);
-                        if (ImGui.InputInt($"Duration (Milliseconds)##{i}", ref duration, 10, 100))
-                        {
-                            var newStep = new Step(duration, step.Intensity);
-                            selected.Steps[i] = newStep;
-                            dataManager.SavePattern(selected);
-                        }
-                        ImGui.SameLine();
-                        if (ImGui.Button($"Delete##{i}"))
-                        {
-                            selected.Steps.RemoveAt(i);
-                            dataManager.SavePattern(selected);
-                        }
-
-                        if (i == selected.Steps.Count - 1) ImGui.BeginDisabled();
-                        if (ImGui.Button("v"))
-                        {
-                            var temp = selected.Steps[i + 1];
-                            selected.Steps[i + 1] = step;
-                            selected.Steps[i] = temp;
-                        }
-                        if (i == selected.Steps.Count - 1) ImGui.EndDisabled();
-                        ImGui.SameLine();
-                        ImGui.SetNextItemWidth(100f);
-                        if (ImGui.InputInt($"Intensity (0-100)##{i}", ref intensity, 1, 10))
-                        {
-                            var newStep = new Step(step.Duration, intensity);
-                            selected.Steps[i] = newStep;
-                            dataManager.SavePattern(selected);
-                        }
-                        ImGui.EndGroup();
-
-                        var rightPadding = ImGui.GetScrollMaxY() > 0.0f ? 20.0f : 5.0f;
-                        // Draw a border around the group
-                        var maxPos = ImGui.GetItemRectMax();
-                        minPos.X -= 0.0f;
-                        minPos.Y -= 4.0f;
-                        maxPos.X = ImGui.GetWindowPos().X + ImGui.GetWindowSize().X - rightPadding;
-                        maxPos.Y += 4.0f;
-                        uint borderColor = ImGui.ColorConvertFloat4ToU32(new Vector4(1.0f, 1.0f, 1.0f, 1.0f));
-                        ImGui.GetWindowDrawList().AddRect(minPos, maxPos, borderColor, 0.0f, ImDrawFlags.RoundCornersAll, 1.0f);
-                        // Add some spacing between the groups
-                        ImGui.SetCursorScreenPos(ImGui.GetCursorScreenPos() + new Vector2(0.0f, 6.0f));
-                    }
-                    ImGui.EndChild();
-
-                    if (ImGui.Button("Add Step"))
-                    {
-                        var newStep = new Step(500, 25);
-                        selected.Steps.Add(newStep);
-                        dataManager.SavePattern(selected);
-                    }
-                    ImGui.SameLine();
-                    if (ImGui.Button("Test Pattern"))
-                    {
-                        toyManager.playPattern(selected);
-                    }
-                }
+                DrawPatternDetail();
 
                 ImGui.EndTable();
             }
         }
 
-        public void DrawPatternDetails()
+        public void DrawPatternList()
         {
+            if (ImGui.Button("New Pattern"))
+            {
+                Pattern p = new Pattern([
+                        new Step(500, 25)
+                        ], "New Pattern");
+                dataManager.SavePattern(p);
+                selectedPatternIndex = dataManager.Patterns.IndexOf(p);
+            }
 
+            if (ImGui.BeginListBox("##PatternList", new Vector2(200f, ImGui.GetWindowHeight() - 85)))
+            {
+                for (var i = 0; i < dataManager.Patterns.Count; i++)
+                {
+                    var pattern = dataManager.Patterns[i];
+                    bool isSelected = selectedPatternIndex == i;
+
+                    string label = $"{pattern.Name}##{pattern.Id}";
+                    if (ImGui.Selectable(label, isSelected))
+                    {
+                        selectedPatternIndex = i;
+                    }
+
+                    if (isSelected)
+                    {
+                        ImGui.SetItemDefaultFocus();
+                    }
+                }
+                ImGui.EndListBox();
+            }
+        }
+
+        public void DrawPatternDetail()
+        {
+            if (selectedPatternIndex > -1 && selectedPatternIndex < dataManager.Patterns.Count)
+            {
+                var selected = dataManager.Patterns[selectedPatternIndex];
+                var name = selected.Name;
+                var id = selected.Id.ToString();
+
+                // Pattern details
+                ImGui.SetNextItemWidth(250f);
+                ImGui.InputText("Id", ref id, 100, ImGuiInputTextFlags.ReadOnly);
+                ImGui.SetNextItemWidth(250f);
+                if (ImGui.InputText("Name", ref name, 40))
+                {
+                    selected.Name = name;
+                    dataManager.SavePattern(selected);
+                }
+
+                // Begin Scrollable area for steps
+                ImGui.BeginChild("PatternDetailsChild", new Vector2(0, ImGui.GetWindowHeight() - 150), true);
+                ImGui.SetCursorScreenPos(ImGui.GetCursorScreenPos() + new Vector2(0.0f, 6.0f));
+
+                // Steps
+                for (var i = 0; i < selected.Steps.Count; i++)
+                {
+                    DrawStepDetail(selected, i);
+                }
+
+                // End Scrollable area for steps
+                ImGui.EndChild();
+
+                if (ImGui.Button("Add Step"))
+                {
+                    var newStep = new Step(500, 25);
+                    selected.Steps.Add(newStep);
+                    dataManager.SavePattern(selected);
+                }
+                ImGui.SameLine();
+                if (ImGui.Button("Test Pattern"))
+                {
+                    toyManager.playPattern(selected);
+                }
+            }
+        }
+
+        public void DrawStepDetail(Pattern selected, int i)
+        {
+            var step = selected.Steps[i];
+            var intensity = step.Intensity;
+            var duration = step.Duration;
+            var minPos = ImGui.GetCursorScreenPos();
+
+            ImGui.BeginGroup();
+            DrawStepDetailTopRow(selected, step, i, ref duration);
+            DrawStepDetailBottomRow(selected, step, i, ref intensity);
+            ImGui.EndGroup();
+
+            DrawPatternDetailBorder(minPos);
+            // Add some spacing between the groups
+            ImGui.SetCursorScreenPos(ImGui.GetCursorScreenPos() + new Vector2(0.0f, 6.0f));
+        }
+
+        private void DrawStepDetailTopRow(Pattern selected, Step step, int i, ref int duration)
+        {
+            if (i == 0) ImGui.BeginDisabled();
+            if (ImGui.Button($"^##{i}"))
+            {
+                var temp = selected.Steps[i - 1];
+                selected.Steps[i - 1] = step;
+                selected.Steps[i] = temp;
+            }
+            if (i == 0) ImGui.EndDisabled();
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100f);
+            if (ImGui.InputInt($"Duration (Milliseconds)##{i}", ref duration, 50, 250))
+            {
+                var newStep = new Step(duration, step.Intensity);
+                selected.Steps[i] = newStep;
+                dataManager.SavePattern(selected);
+            }
+            ImGui.SameLine();
+            if (ImGui.Button($"Delete##{i}"))
+            {
+                selected.Steps.RemoveAt(i);
+                dataManager.SavePattern(selected);
+            }
+        }
+
+        private void DrawStepDetailBottomRow(Pattern selected, Step step, int i, ref int intensity)
+        {
+            if (i == selected.Steps.Count - 1) ImGui.BeginDisabled();
+            if (ImGui.Button($"v##{i}"))
+            {
+                var temp = selected.Steps[i + 1];
+                selected.Steps[i + 1] = step;
+                selected.Steps[i] = temp;
+            }
+            if (i == selected.Steps.Count - 1) ImGui.EndDisabled();
+            ImGui.SameLine();
+            ImGui.SetNextItemWidth(100f);
+            if (ImGui.InputInt($"Intensity (0-100)##{i}", ref intensity, 1, 10))
+            {
+                var newStep = new Step(step.Duration, intensity);
+                selected.Steps[i] = newStep;
+                dataManager.SavePattern(selected);
+            }
+        }
+
+        private void DrawPatternDetailBorder(Vector2 minPos)
+        {
+            var rightPadding = ImGui.GetScrollMaxY() > 0.0f ? 20.0f : 5.0f;
+            var maxPos = ImGui.GetItemRectMax();
+            minPos.X -= 0.0f;
+            minPos.Y -= 4.0f;
+            maxPos.X = ImGui.GetWindowPos().X + ImGui.GetWindowSize().X - rightPadding;
+            maxPos.Y += 4.0f;
+            uint borderColor = ImGui.ColorConvertFloat4ToU32(new Vector4(1.0f, 1.0f, 1.0f, 1.0f));
+            ImGui.GetWindowDrawList().AddRect(minPos, maxPos, borderColor, 0.0f, ImDrawFlags.RoundCornersAll, 1.0f);
         }
     }
 }
